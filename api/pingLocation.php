e641ca9299108a38093536701e1856a73a56b175
pierater <martin.almaraz@gmail.com>
2016-08-22 22:25:07 -0700

    forgot an & when starting notifierService

--- api/pingLocation.php
+++ api/pingLocation.php
@@ -3,7 +3,7 @@ include "../serverConnect/dbConnect.php";
 $id = $_GET["id"];
 $lat = $_GET["lat"];
 $lon = $_GET["lon"];
-if(isUserId($id))
+if(!isUserId($id))
 {
 	print_r(json_encode(array("error" => "true"));
 }
@@ -11,7 +11,7 @@ else
 {
 	$sql = "UPDATE `userLocations` SET `lat` = '$lat', `lon` = '$lon' WHERE `id` = '$id'";
 	$query = $conn->query($sql);
-	$command = escapeshellcmd("python ../matchMakerServer/matchMakerService.py $id $lat $lon");
+	$command = escapeshellcmd("python ../matchMakerServer/matchMakerService.py $id $lat $lon &");
 	$output = shell_exec($command);
 }
 ?>
